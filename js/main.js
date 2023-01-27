5952d9ee1a683c9b76a868a45fec8f1af9b565b2
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2023-01-27 19:54:05 +0100

    Changed the entire page and added some styles for the images with animation

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
 "use strict";
-
-const button = document.querySelector('.js-button');
-
-function handleClickButton(ev) {
-    ev.preventDefault();
-    button.classList.toggle("change-styles-click");
-}
-
-button.addEventListener("click", handleClickButton)
